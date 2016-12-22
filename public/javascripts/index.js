253d683f9d88618a12d1a39d7b1cd61a27047c73
17thFox <elisabeta.vulpe@gmail.com>
2016-12-22 13:12:05 +0200

    Updating status done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,40 +1,76 @@
 (function() {
     'use strict';
 
-
     var $todos = $('#todos');
 
     function myLi(item) {
-        var $li = $('<li class="list-group-item">');
+        var $li = $('<li class="list-group-item clearfix">');
+        // $li.data('todoId', item.id);
+
         var $doneTodos = $('#doneTodos');
 
-        var $title = $('<span>').text(item.id + ': ' + item.title + '  ');
+        var $title = $('<span data-editable>').text(item.title);
         $li.append($title);
 
-        var $doneButton = $('<button type="button" class="btn btn-info">').text('DONE');
-        var $deleteButton = $('<button type="button" class="btn btn-danger">').text('DELETE');
-        var $notDoneButton;
+        var $doneButton = $('<button type="button" class="btn btn-info pull-right">').text('DONE');
+        var $deleteButton = $('<button type="button" class="btn btn-danger pull-right">').text('DELETE');
+        var $notDoneButton = $('<button type="button" class="btn btn-info pull-right">').text('NOT-DONE');
+
+        $li.on('click', '[data-editable]', function() {
+            var $el = $(this);
+
+            var $input = $('<input/>').val($el.text());
+            $el.replaceWith($input);
+
+            var save = function() {
+
+                console.log(item.id + '  this is item iiiidddddd ')
+                if ($input.val().length === 0) {
+                    deleteTodo(item.id, function() {
+                        $li.remove();
+                    });
+                } else {
+                    item.title = $input.val();
+                    var $span = $('<span data-editable />').text($input.val());
+                    $input.replaceWith($span);
+                    updateTitle(item.id, item.title, item.status);
+                }
+            };
+            $input.on('blur', save).focus();
+        });
 
         $doneButton.on('click', function() {
-            $notDoneButton = $doneButton.text('NOT-DONE');
+            item.status = 'done';
+            updateTitle(item.id, item.title, item.status);
+            $doneButton.hide();
+            $notDoneButton.show();
+            $li.append($notDoneButton);
             $doneTodos.append($li);
         });
 
-        // $notDoneButton.on('click', function() {
-        //     $doneButton = $notDoneButton.text('DONE');
-        // });
+        $notDoneButton.on('click', function() {
+            item.status = 'not-done';
+            updateTitle(item.id, item.title, item.status);
+            $notDoneButton.hide();
+            $doneButton.show();
+            $todos.append($li);
+        });
 
         $deleteButton.on('click', function() {
             deleteTodo(item.id, function() {
                 $li.remove();
             });
-
         });
 
-        $li.append($doneButton);
         $li.append($deleteButton);
 
-        $todos.append($li);
+        if (item.status === 'not-done') {
+            $li.append($doneButton);
+            $todos.append($li);
+        } else {
+            $li.append($notDoneButton);
+            $doneTodos.append($li);
+        }
     }
 
 
@@ -61,12 +97,10 @@
             });
         });
 
-
     $(function() {
         $('#addTodosForm').on('submit', function(event) {
             event.preventDefault();
             var myAddTodosInput = $('#addTodosInput').val();
-
 
             $.ajax({
                     method: 'POST',
@@ -81,13 +115,16 @@
         })
     })
 
-
-    // $.ajax({
-    //         method: "PUT",
-    //         url: "/api/todos",
-    //         data: {},
-    //     })
-    //     .done()
-
-
+    function updateTitle(myId, myTitle, myStatus) {
+        $.ajax({
+                method: "PUT",
+                url: "/api/todos",
+                data: {
+                    id: myId,
+                    title: myTitle,
+                    status: myStatus
+                },
+            })
+            .done();
+    }
 })();
