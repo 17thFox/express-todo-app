c49d13a04e696e8938704ef4aa00058b061bf294
17thFox <elisabeta.vulpe@gmail.com>
2017-01-06 16:09:07 +0200

    modified new redis()

--- redis-storage.js
+++ redis-storage.js
@@ -4,10 +4,8 @@ var Redis = require('ioredis');
 
 var redis = new Redis({
     port: 16049, // Redis port
-    host: 'ec2-54-225-127-4.compute-1.amazonaws.com' // Redis host
-    family: 4, // 4 (IPv4) or 6 (IPv6)
-    password: 'p6dd5e5ee0eaface1df6ef17b57ae6ef986955642b9e877ef67298d90591ab045',
-    db: 0
+    host: 'ec2-54-225-127-4.compute-1.amazonaws.com', // Redis host
+    password: 'p6dd5e5ee0eaface1df6ef17b57ae6ef986955642b9e877ef67298d90591ab045'
 });
 
 
